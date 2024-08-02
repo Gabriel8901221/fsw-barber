eb737d201cd866a28bd66b97b9da9bf7dbd127d5
gabriel <gabrielcardosog990@gmail.com>
2024-08-02 16:28:04 -0400

    chore: add git hooks setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-import Image from "next/image";
-import { Button } from "./_components/ui/button";
+import { Button } from "./_components/ui/button"
 
 export default function Home() {
-  return (
-   <Button>
-    Hello FSW
-   </Button>
-  );
+  return <Button>Hello FSW</Button>
 }
